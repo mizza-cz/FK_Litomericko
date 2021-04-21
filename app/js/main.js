f0fe897f7b9357e8e1d866ce796d36f6a40010e8
mizza-cz <mizza.cz666@gmail.com>
2021-04-21 17:44:59 +0200

    add topstory and gameslider

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,30 @@ $(function () {
   $('.nav__link').on('click', function () {
     $(this).closest('.nav__item').toggleClass('active');
   });
+  $('.gameslider__inner').slick({
+    autoplay: true,
+    prevArrow:
+      '<button class="gameslider__btn gameslider__btn--prev"><img src="images/svg/arrow-left.svg" alt="" ></button> ',
+    nextArrow:
+      ' <button class="gameslider__btn gameslider__btn--next"><img src="images/svg/arrow-right.svg" alt = "" ></button > ',
+    responsive: [
+      {
+        breakpoint: 901,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
+$(document).ready(function () {
+  $('#hpArticle .hp-article').hover(
+    function () {
+      $('#hpArticle .hp-article').removeClass('topArticleActive');
+      $(this).addClass('topArticleActive');
+    },
+    function () {},
+  );
 });
 
 const menuBtn = document.querySelector('.navbar__menu');
