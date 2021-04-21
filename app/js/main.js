fbcecaec514a62525b2b06c75599970e5383c272
mizza-cz <mizza.cz666@gmail.com>
2021-04-21 22:15:39 +0200

    add footer/partners/clanky section

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,67 @@ $(function () {
       },
     ],
   });
+  $('.result-slider__inner').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+
+    prevArrow:
+      '<button class="gameslider__btn gameslider__btn--prev"><img src="images/svg/arrow-left.svg" alt="" ></button> ',
+    nextArrow:
+      ' <button class="gameslider__btn gameslider__btn--next"><img src="images/svg/arrow-right.svg" alt = "" ></button > ',
+    responsive: [
+      {
+        breakpoint: 1131,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $('.partner-slider__inner').slick({
+    slidesToShow: 8,
+    slidesToScroll: 8,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1370,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        },
+      },
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 561,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
 });
 $(document).ready(function () {
   $('#hpArticle .hp-article').hover(
